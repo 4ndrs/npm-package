766fdfd41dace8434b052f79e7060a702773ea57
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-25 17:29:55 +0100

    Add argument parsing with yargs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import yargs from "yargs/yargs";
+
 export const main = () => {
-  console.log("Hello, World!");
+  const args = yargs(process.argv.slice(2))
+    .options({ name: { type: "string", default: "World" } })
+    .parseSync();
+
+  const { name } = args;
+
+  console.info(`Hello, ${name}!`);
 };
